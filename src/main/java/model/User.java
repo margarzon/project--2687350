f271ad62ab74a19ae4b6c2d3ec24111597e723ca
margarzon <125483628+margarzon@users.noreply.github.com>
2023-05-12 17:16:57 -0500

    Add files via upload

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,13 +1,22 @@
 package model;
 
 public class User {
+    // atributos
     private Integer user_id;
     private String user_firstname;
     private String user_lastname;
     private String user_email;
     private String user_password;
 
-    public User(Integer user_id, String user_firstname,String user_lastname, String user_email, String user_password) {
+
+
+    // Constructores
+
+    public User() {
+
+    }
+
+    public User(Integer user_id, String user_firstname, String user_lastname, String user_email, String user_password) {
         this.user_id = user_id;
         this.user_firstname = user_firstname;
         this.user_lastname = user_lastname;
@@ -15,10 +24,49 @@ public class User {
         this.user_password = user_password;
     }
 
+    // GETTERS
     public Integer getUser_id() {
         return user_id;
     }
 
+    public String getUser_firstname() {
+        return user_firstname;
+    }
+
+    public String getUser_lastname() {
+        return user_lastname;
+    }
+
+    public String getUser_email() {
+        return user_email;
+    }
+
+    public String getUser_password() {
+        return user_password;
+    }
+
+    //SETTERS
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public void setUser_firstname(String user_firstname) {
+        this.user_firstname = user_firstname;
+    }
+
+    public void setUser_lastname(String user_lastname) {
+        this.user_lastname = user_lastname;
+    }
+
+    public void setUser_email(String user_email) {
+        this.user_email = user_email;
+    }
+
+    public void setUser_password(String user_password) {
+        this.user_password = user_password;
+    }
+
+    // ToString
     @Override
     public String toString() {
         return "User{" +
@@ -29,4 +77,6 @@ public class User {
                 ", user_password='" + user_password + '\'' +
                 '}';
     }
+
+
 }
