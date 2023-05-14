5c033a66dd5d1e8d92c33ee67288829f571e92be
margarzon <125483628+margarzon@users.noreply.github.com>
2023-05-13 23:17:33 -0500

    Add files via upload

---
+++ src/main/java/co/edu/sena/project2687350/util/BasicConnectionWithResources.java
@@ -0,0 +1,31 @@
+package co.edu.sena.project2687350.util;
+
+import java.sql.*;
+
+public class BasicConnectionWithResources
+{
+    public static void main(String[] args)
+    {
+        String url =
+                "jdbc:mysql://localhost:3306/my_app?serverTimezone=America/Bogota";
+        String username = "root";
+        String password = "Mar.1525";
+        String sql = "SELECT * FROM myapp.users_tbl";
+        try (Connection conn =
+                     DriverManager.getConnection(url,
+                             username, password);
+             Statement stmt =
+                     conn.createStatement();
+             ResultSet rs =
+                     stmt.executeQuery(sql)) {
+            while (rs.next()) {
+
+                System.out.println(rs.getString("user_firstname"));
+
+                        System.out.println(rs.getString("user_lastname"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } // end try-catch
+    } // main
+} // BasicConnectionWithResources
